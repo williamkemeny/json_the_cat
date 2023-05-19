f44ff2313e4b56602a4f712a4df82201f14d8fab
William Kemeny <williamkemeny@gmail.com>
2023-05-19 15:12:36 -0400

    Refactored code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,26 @@
 const request = require("request");
 const commandLine = process.argv.slice(2);
 
-request(
-  "https://api.thecatapi.com/v1/breeds/search?q=" + commandLine[0],
-  (error, response, body) => {
-    const data = JSON.parse(body);
-    if (data[0] !== undefined) {
-      console.log(data[0].description);
-    } else if (commandLine[0] !== undefined) {
-      console.log(`Breed ${commandLine[0]} not found.`);
-    } else {
-      console.log("error:", error); // Print the error if one occurred
-      console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+const fetchBreedDescription = function (breedName, callback) {
+  request(
+    "https://api.thecatapi.com/v1/breeds/search?q=" + breedName,
+    (error, response, body) => {
+      const data = JSON.parse(body);
+      if (error) {
+        callback(error, null); // Print the error if one occurred
+        return null;
+      }
+      if (response.statusCode !== 200) {
+        callback("statusCode:" + response.statusCode, null); // Print the response status code if a response was received
+        return;
+      } else {
+        if (data.lenghth === 0) {
+          console.log(`Breed ${breedName} not found.`);
+        }
+      }
+      callback(null, data[0].description);
     }
-  }
-);
+  );
+};
+
+module.exports = { fetchBreedDescription };
